7c2379abc20cb807a83ff25e3942b4aa21b2f9ae
刘籽漪 <liuziyi@liuziyideMacBook-Air.local>
2026-02-17 22:27:55 +0100

    fix render port

--- app.py
+++ app.py
@@ -202,7 +202,9 @@ def logout():
 # Run
 # ======================
 
+import os
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
